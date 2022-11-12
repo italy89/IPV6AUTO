06084e755760f40228a6e11e06511fe931f08a41
italy89 <39641172+italy89@users.noreply.github.com>
2022-11-12 20:34:23 +0700

    Update ipv6.sh

--- ipv6.sh
+++ ipv6.sh
@@ -28,16 +28,11 @@ install_3proxy() {
 gen_3proxy() {
     cat <<EOF
 daemon
-maxconn 10000
-nserver 1.1.1.1
-nserver 8.8.4.4
-nserver 2001:4860:4860::8888
-nserver 2001:4860:4860::8844
+maxconn 2000
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
-stacksize 6291456
 flush
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 "::" $2 " "}' ${WORKDATA})
 $(awk -F "/" '{print "auth none\n" \
